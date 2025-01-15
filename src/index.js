f38784e71aa0c325be3a8316d9fa82a9b9e2f725
nithla <nithilareddy@yahoo.com>
2025-01-15 23:48:37 +0530

    Header changes.

--- src/index.js
+++ src/index.js
@@ -1,28 +1,34 @@
 /* Styles */
 import './styles/reset.scss'
 import './styles/variables.scss'
-import './styles/fonts.scss'
 import './styles/main.scss'
 import './styles/header.scss'
 import './styles/footer.scss'
-import './styles/blog.scss'
-import './styles/archive.scss'
 import './styles/home.scss'
+import './styles/pivot.scss'
+import './styles/skills.scss'
+import './styles/work.scss'
 
 /* JavaScript */
 import './javascript/carousel.js'
 
 /* Assets */
-import './assets/1-crop.jpg';
-import './assets/1.jpg';
-import './assets/2.jpg'; 
-import './assets/3.jpg'; 
-import './assets/4.jpg';
-import './assets/5.png';
-import './assets/6.png';
-import './assets/instagram.png';
-import './assets/letterboxd.svg';
-import './assets/linkedin.png';
+import './assets/personal-pictures/1.png';
+import './assets/personal-pictures/2.jpg';
+
+import './assets/social-logos/instagram.png';
+import './assets/social-logos/letterboxd.svg';
+import './assets/social-logos/linkedin.png';
+
+import './assets/project-logos/canopygrowth.png'
+import './assets/project-logos/dupont.png'
+import './assets/project-logos/eon.png'
+import './assets/project-logos/equinix.png'
+import './assets/project-logos/myntra.png'
+import './assets/project-logos/tadigital.png'
+import './assets/project-logos/thoughtworks.png'
+import './assets/project-logos/accenture.png';
+import './assets/project-logos/bt.png';
 
 
 
--- src/javascript/carousel.js
+++ src/javascript/carousel.js
@@ -2,22 +2,12 @@ import $ from 'jquery';
 window.$ = window.jQuery = $;
 import 'slick-carousel';
 
-$('.archive__card-wrapper').slick({
-    prevArrow: '<i class="fa fa-angle-left prev" aria-hidden="true"></i>',
-    nextArrow: '<i class="fa fa-angle-right next" aria-hidden="true"></i>',
-    slidesToShow: 3,
-    responsive: [
-        {
-            breakpoint: 768,
-            settings: {
-                slidesToShow: 2
-            }
-        },
-        {
-            breakpoint: 576,
-            settings: {
-                slidesToShow: 1
-            }
-        }
-    ]
+$('.work__carousel').slick({
+    speed: 6000,
+    autoplay: true,
+    autoplaySpeed: 0,
+    cssEase: 'linear',
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    variableWidth: true,
 });
